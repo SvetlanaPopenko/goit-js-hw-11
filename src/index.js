7c83a2e54f58478436456da8a0775a38f1f3b264
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-27 13:35:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    gallary: document.querySelector('.gallery'),
+}
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(evt) {
+    evt.preventDefault();
+
+}
+
+const options = {
+    headers:{
+Authorization:
+    }
+}
