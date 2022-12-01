6a1bc7f760159f77d380876e49782bb6a18773bb
SvetlanaPopenko <spopenko02@gmail.com>
2022-12-01 02:01:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import './css/styles.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31629453-3aa42bb9ff6dc8c3c3e379cd8';
 const PER_PAGE = 40;
 
 let searchQuery = '';
-let pageNumber = 1;
+let page = 1;
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   guard: document.querySelector('.js-guard'),
-  };
-console.log(refs.searchForm)
+};
+
 const options = {
   root: null,
   rootMargin: '200px',
   threshold: 1.0,
-  }
+};
+
 const observer = new IntersectionObserver(onLoad, options);
 
 // refs.searchForm.addEventListener('submit', onSearch);
@@ -34,25 +35,26 @@ const observer = new IntersectionObserver(onLoad, options);
 //     return;
 //     notifyInfo();
 //   }
-  
+
 // }
 
-
-
-function fetchPhotoApi(searchValue, pageNumber = 1) {
-  return fetch(`${BASE_URL}?key=${API_KEY}&q=${searchValue}&page=${pageNumber}&per_page=${PER_PAGE}`)
-    .then(resp => {
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-      return resp.json();
-    })
+function fetchPhotoApi(searchValue, page = 1) {
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${searchValue}&page=${page}&per_page=${PER_PAGE}`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
     }
+    return resp.json();
+  });
+}
 
-fetchPhotoApi().then(data => {
-  refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits))
-    observer.observe(refs.guard)
-}).catch(error => console.error(error));
+fetchPhotoApi()
+  .then(data => {
+    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+    observer.observe(refs.guard);
+  })
+  .catch(error => console.error(error));
 
 export function createMarkup(arr) {
   return arr
@@ -65,7 +67,7 @@ export function createMarkup(arr) {
         views,
         comments,
         downloads,
-      }) => 
+      }) =>
         `<a href="${largeImageURL}"><div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
@@ -82,23 +84,27 @@ export function createMarkup(arr) {
       <b>Downloads</b>${downloads}
     </p>
   </div>
-</div></a>`)
+</div></a>`
+    )
     .join('');
-  }
+}
 
-  // function addMarkup(arr) {
-  //   refs.gallery.insertAdjacentHTML('beforeend', createMarkup(arr))
-  // }
+// function addMarkup(arr) {
+//   refs.gallery.insertAdjacentHTML('beforeend', createMarkup(arr))
+// }
 
 function onLoad(entries, observer) {
-  console.log(entries);
-  //  pageNumber += 1;
-  //  fetchPhotoApi(pageNumber).then(data => {
-  //    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-  //    if (data.pageNumber === data.pages) {
-  //      refs.loadMore.setAttribute('hidden', true)
-  //    }
-  //  })
-  //  .catch(error => console.error(error));
- }
-
+ 
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      fetchPhotoApi(page).then(data => {
+        refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+        if (data.page === data.pages) {
+       observer.unobserve(refs.guard);
+     }
+      });
+    }
+  });
+   console.log(entries);
+}
