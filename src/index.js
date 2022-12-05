12de7ca67be796598fcb53a1dc7b10c82f1ca2b8
SvetlanaPopenko <spopenko02@gmail.com>
2022-12-05 02:26:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,59 @@
 import './css/styles.css';
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import axios from 'axios';
+import { createMarkup } from './modules.js/markup';
+import { PER_PAGE, fetchPhotoApi } from './modules.js/fetch';
+import { simpleGallery } from './modules.js/simplelightbox';
+import { refs } from './modules.js/refs';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '31629453-3aa42bb9ff6dc8c3c3e379cd8';
 
-const searchParams = new URLSearchParams({
-  per_page: 40,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-});
+import {
+  notifyFailure,
+  notifyInfo,
+  notifySuccess,
+  notifyInfoSearch,
+} from './modules.js/notify';
+
+
 
 let searchQuery = '';
 let page = 1;
+let totalHits = '';
 
-const refs = {
-  searchForm: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
-  guard: document.querySelector('.js-guard'),
-};
+refs.searchForm.addEventListener('submit', onSearch);
+
+async function onSearch(evt) {
+  evt.preventDefault();
+  page = 1;
+  observer.unobserve(refs.guard);
+  searchQuery = evt.currentTarget.searchQuery.value.trim();
+  refs.gallery.innerHTML = '';
+  evt.currentTarget.reset();
+
+  if (!searchQuery) {
+    refs.gallery.innerHTML = '';
+    notifyInfoSearch();
+    return;
+  }
+
+  await fetchPhotoApi(searchQuery, page)
+    .then(gallery => {
+      totalHits = gallery.data.totalHits;
+
+      if (!totalHits) {
+        return notifyFailure();
+      }
+      notifySuccess(totalHits);
+      addMarkup(gallery.data.hits);
+      simpleGallery.refresh();
+      
+      observer.observe(refs.guard);
+    })
+    .catch(error => console.log(error));
+}
+
+function addMarkup(arr) {
+  refs.gallery.insertAdjacentHTML('beforeend', createMarkup(arr));
+}
 
 const options = {
   root: null,
@@ -32,86 +63,31 @@ const options = {
 
 const observer = new IntersectionObserver(onLoad, options);
 
-// refs.searchForm.addEventListener('submit', onSearch);
-
-// function onSearch(evt) {
-//   evt.preventDefault();
-//     searchQuery = evt.currentTarget.elements.value;
-
-//   if (!searchQuery) {
-//     return;
-//     notifyInfo();
-//   }
-
-// }
-
-async function fetchPhotoApi(searchValue, page = 1) {
-  try {
-    const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${searchParams}&page=${page}`
-    );
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-fetchPhotoApi()
-  .then(data => {
-    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-    observer.observe(refs.guard);
-  })
-  .catch(error => console.error(error));
-
-export function createMarkup(arr) {
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<a href="${largeImageURL}">
-        <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>${downloads}
-    </p>
-  </div>
-</div></a>`
-    )
-    .join('');
-}
-
-// function addMarkup(arr) {
-//   refs.gallery.insertAdjacentHTML('beforeend', createMarkup(arr))
-// }
-
-function onLoad(entries, observer) {
-  entries.forEach(entry => {
+async function onLoad(entries, observer) {
+  await entries.forEach(entry => {
     if (entry.isIntersecting) {
       page += 1;
-      fetchPhotoApi(page).then(data => {
-        refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-        if (data.page === data.pages) {
-          observer.unobserve(refs.guard);
-        }
+
+      fetchPhotoApi(searchQuery, page).then(gallery => {
+        addMarkup(gallery.data.hits);
+        simpleGallery.refresh();
+
+        const { height: cardHeight } = document
+          .querySelector(".gallery")
+          .firstElementChild.getBoundingClientRect();
+
+        window.scrollBy({
+          top: cardHeight * 2,
+          behavior: "smooth",
+        });
+      }).catch(error => {
+        console.log(error);
       });
+      if (page === Math.round(totalHits / PER_PAGE)) {
+        observer.unobserve(refs.guard);
+        page = 1;
+      }
     }
   });
-  console.log(entries);
 }
+  
