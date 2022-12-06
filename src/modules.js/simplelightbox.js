c686476293e0a6ad374dd2ff19af8198b4a160b0
SvetlanaPopenko <spopenko02@gmail.com>
2022-12-06 16:24:57 +0200

    Delete simplelightbox.js

--- src/modules.js/simplelightbox.js
+++
@@ -1,8 +0,0 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-export let simpleGallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});
